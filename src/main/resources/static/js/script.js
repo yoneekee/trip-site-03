5f076ac7e7c88c1dd5d952f57b39e7f568554c3b
yoneekee <yoneeki1177@gmail.com>
2023-04-07 15:48:03 +0900

    java

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -48,6 +48,29 @@ $("#btnDeleteAjax").on("click", function (e) {
   return false;
 });
 
+$("#btnDeleteBoardAjax").on("click", function (e) {
+  //e.preventDefault();
+  isState = "delete";
+  $("#deleteBoardModal .modal-title").text("Enter password to delete your board content.");
+  deleteBoardModal.show();
+  return false;
+});
+
+$("#btnDeleteBoardAjax").on("click", function (e) {
+  //e.preventDefault();
+  isState = "delete";
+  $("#deleteBoardModal .modal-title").text("Enter password to delete your board content.");
+  deleteBoardModal.show();
+  return false;
+});
+
+$("#btnVipDeleteBoardAjax").on("click", function (e) {
+  //e.preventDefault();
+  isState = "delete";
+  $("#deleteVipBoardModal .modal-title").text("Enter password to delete your board content (vip)");
+  deleteVipBoardModal.show();
+  return false;
+});
 $("#btnDeleteConfirm").on("click", function (e) {
   $.ajax({
     url: "/member/deleteProcess", // 해당 함수 필요 (responsebody)
@@ -65,3 +88,64 @@ $("#btnDeleteConfirm").on("click", function (e) {
     },
   });
 });
+
+/*
+$("#btnBoardDeleteConfirm").on("click", function (e) {
+  $.ajax({
+    url: "/board/freeboarddelete", // 해당 함수 필요 (responsebody)
+    data: { no: session.no, userPw: $("#userPw_check").val() },
+    type: "POST",
+    success: function (response) {
+      console.log(response);
+      if (response.msg === "ok") {
+        setTimeout(function () {
+          location.href = "/";
+        }, 500);
+      } else {
+        location.reload();
+      }
+    },
+  });
+});
+
+
+$("#btnVipBoardDeleteConfirm").on("click", function (e) {
+  $.ajax({
+    url: "/board/vipboarddelete", // 해당 함수 필요 (responsebody)
+    data: { no: session.no, userPw: $("#userPw_check").val() },
+    type: "POST",
+    success: function (response) {
+      console.log(response);
+      if (response.msg === "ok") {
+        setTimeout(function () {
+          location.href = "/";
+        }, 500);
+      } else {
+        location.reload();
+      }
+    },
+  });
+});
+*/
+
+/////////////////////////////////////////////////////////////////////
+/*
+          @PostMapping("/deleteProcess")
+          @ResponseBody
+          public Map<String, Object> deleteProcess(
+          MemberDto memberDto,
+          HttpServletRequest request,
+          RedirectAttributes redirectAttributes)  {
+          int result = memberService.deleteMember(memberDto);
+          System.out.println(result);
+          Map<String, Object> sendJson = new HashMap<>();
+          if (result > 0) {
+            HttpSession session = request.getSession();
+            session.removeAttribute("loggedMember");
+            sendJson.put("msg", "ok");
+          } else {
+            sendJson.put("msg", "fail");
+          }
+          return sendJson;
+          }
+      */
